3954bd20f1e40dfcd8d18c33bb960d183c123c16
Albert Prince Mensah <apmensah@gmail.com>
2023-07-25 17:37:58 +0000

    updated urls.py and views.py

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -7,4 +7,7 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, title):
+    return render(request, f"encyclopedia/entry/{title}.html")
 
