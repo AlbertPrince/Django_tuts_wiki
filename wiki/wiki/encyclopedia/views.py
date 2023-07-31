72d7dc936833052b54940d1b2f5e7173d0bacc81
Albert Prince Mensah <apmensah@gmail.com>
2023-07-31 22:29:15 +0000

    added hyperlinks

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -9,5 +12,11 @@ def index(request):
     })
     
 def entry(request, title):
-    return render(request, f"encyclopedia/entry/{title}.html")
-
+    markdown_entry = util.get_entry(title)
+    entry = markdown2.markdown(markdown_entry)
+    if not entry:
+        return HttpResponseNotFound("<h1>Entry not found</h1>")
+    return render(request, f"encyclopedia/entry.html",{
+        "entry" : mark_safe(entry),
+        "title" : title
+    })
