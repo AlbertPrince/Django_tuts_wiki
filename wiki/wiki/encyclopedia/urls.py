6875cef89d308e250357a5393ff7f5b758635a9b
Albert Prince Mensah <apmensah@gmail.com>
2023-08-01 10:58:15 +0000

    added random view

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 # app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("random/", views.random_entry, name="random_entry"),
+    path("create/", views.create_entry, name="create"),
+    path("wiki/<str:title>", views.edit_entry, name="edit")
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,9 +1,16 @@
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from . import util
 import markdown2
+import random
+
+class Entry_Form(forms.Form):
+    title = forms.CharField(label="Title", max_length=20)
+    entry = forms.CharField(label="Your Entry", max_length=200)
 
 
 def index(request):
@@ -14,9 +21,24 @@ def index(request):
 def entry(request, title):
     markdown_entry = util.get_entry(title)
     entry = markdown2.markdown(markdown_entry)
-    if not entry:
-        return HttpResponseNotFound("<h1>Entry not found</h1>")
+    if not markdown_entry:
+        return render(request, "encyclopedia/error.html", {"message": "Entry not found"})
     return render(request, f"encyclopedia/entry.html",{
         "entry" : mark_safe(entry),
         "title" : title
     })
+    
+def random_entry(request):
+    entries = util.list_entries()
+    if entries:
+        random_title = random.choice(entries)
+        return redirect('entry', title=random_title)
+    else:
+        return redirect('index')
+    
+def create_entry(request):
+    return render(request, "encyclopedia/create.html")
+
+def edit_entry(request, title):
+    pass
+    
